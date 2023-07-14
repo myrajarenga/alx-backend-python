06487216e432cd610397593346ec916660cd688e
myrajarenga <myrajarenga1234@gmail.com>
2023-07-14 09:11:04 -0700

    more tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""more tasks """
+
+
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+"""uses task_wait_random"""
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return await asyncio.gather(*tasks)
