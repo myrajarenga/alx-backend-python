eb9e0659ee39f72950473ce0c9c58fa230c64e14
myrajarenga <myrajarenga1234@gmail.com>
2023-07-14 08:04:36 -0700

    async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -14,4 +14,4 @@ async def async_generator() -> [float, None, None]:
     """coroutine called async generator loop 10 times wait  sec"""
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint() * 10
+        yield random.random() * 10
