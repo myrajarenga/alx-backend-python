96c7d066c107c6b49618234875bce9e6355ef059
myrajarenga <myrajarenga1234@gmail.com>
2023-07-14 08:51:57 -0700

    taska

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 import asyncio
 from asyncio import Task
-from typing import Any
 wait_random = __import__('0-basic_async_syntax').wait_random
 """returns a asyncio.Task"""
 
 
-def task_wait_random(max_delay: int) -> Any:
+def task_wait_random(max_delay: int) -> Task:
     '''Return asynchronous taska'''
     loop = asyncio.get_event_loop()
     task = loop.create_task(wait_random(max_delay))
