e8935f8ef6e012873a4449e37fe8703272ace4ef
myrajarenga <myrajarenga1234@gmail.com>
2023-07-31 07:29:19 -0700

    tests raising errors

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,6 +17,28 @@ class TestUtils(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected_result):
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+    @staticmethod
+    def _expected_exception_keyerror(nested_map, path):
+        """using the try and excaption to get expected rseults"""
+        try:
+            access_nested_map(nested_map, path)
+            return None
+        except KeyError as e:
+            return e
+
+    @parameterized.expand([
+        ({}, ["a"], KeyError),
+        ({"a": 1}, ["a", "b"], KeyError),
+    ])
+    def test_access_nested_map_exception\
+            (self, nested_map, path, expected_exception):
+        """testing method to raise exceptio errors"""
+        result = self._expected_exception_keyerror(nested_map, path)
+        self.assertIsInstance(result, expected_exception)
+        if expected_exception is KeyError:
+            self.assertEqual\
+                    (str(result), "Key not found: '{}'".format(path[-1]))
+
 
 if __name__ == "__main__":
     unittest.main()
