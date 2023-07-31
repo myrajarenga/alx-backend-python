4c6cf3d70f44e4d682e409a227d6303f1fd41e71
myrajarenga <myrajarenga1234@gmail.com>
2023-07-31 08:01:19 -0700

    mocking HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-"""
-Implimenting estAccessNestedMap.test_access_nested_map method
-"""
+
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
+from unittest.mock import patch, Mock
 
 
 class TestUtils(unittest.TestCase):
@@ -19,7 +18,7 @@ class TestUtils(unittest.TestCase):
 
     @staticmethod
     def _expected_exception_keyerror(nested_map, path):
-        """using the try and excaption to get expected rseults"""
+        """using the try and exception to get expected results"""
         try:
             access_nested_map(nested_map, path)
             return None
@@ -30,15 +29,35 @@ class TestUtils(unittest.TestCase):
         ({}, ["a"], KeyError),
         ({"a": 1}, ["a", "b"], KeyError),
     ])
-    def test_access_nested_map_exception\
-            (self, nested_map, path, expected_exception):
-        """testing method to raise exceptio errors"""
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception):
         result = self._expected_exception_keyerror(nested_map, path)
         self.assertIsInstance(result, expected_exception)
         if expected_exception is KeyError:
-            self.assertEqual\
-                    (str(result), "Key not found: '{}'".format(path[-1]))
+            self.assertEqual(str(result), f"Key not found: '{path[-1]}'")
+
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('test_utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_requests_get):
+        # Configure the mock_requests_get to return a mock response
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_requests_get.return_value = mock_response
+
+        # Call the get_json function
+        result = get_json(test_url)
+
+        # Assert that the mocked get method was called exactly once with the test_url
+        mock_requests_get.assert_called_once_with(test_url)
+
+        # Assert that the output of get_json is equal to test_payload
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
     unittest.main()
+
